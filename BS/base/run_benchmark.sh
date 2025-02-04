f66b7410b070bd136eaa84a1f62cfa8aa3aabe4d
alejofontalvo <102882477+alejofontalvo@users.noreply.github.com>
2025-02-03 23:42:11 -0500

    Update run_benchmark.sh

--- BS/base/run_benchmark.sh
+++ BS/base/run_benchmark.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
+SOLUTIONS_DIR="/app/soluciones"
+LANGUAGES=("python" "csharp" "go" "java" "javascript")
 
 echo "=== Iniciando Benchmark ==="
-echo "=== Construyendo y ejecutando python ==="
-cd soluciones/python
-docker build -t py .
-TIME_MS=$(docker run --rm py)
+for lang in "${LANGUAGES[@]}"; do
+  FOLDER="$SOLUTIONS_DIR/$lang"
+  if [ -d "$FOLDER" ]; then
+    echo "=== Construyendo y ejecutando $lang ==="
+    cd "$FOLDER"
+    docker build -t "benchmark-$lang" .
+    TIME_MS=$(docker run --rm "benchmark-$lang")
 
-# Extraer el output.txt
-    CID=$(docker create py)
+    # Extraer el output.txt
+    CID=$(docker create "benchmark-$lang")
     docker cp "$CID:/app/output.txt" ./output.txt
     docker rm "$CID"
 
@@ -19,11 +24,10 @@ TIME_MS=$(docker run --rm py)
     echo
     cd /app
   else
-    echo "Carpeta soluciones/python no existe. Se omite."
+    echo "Carpeta $FOLDER no existe. Se omite."
   fi
 done
 echo "=== Benchmark completado ==="
-
 
 
 
